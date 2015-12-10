7f7e978d50929d7feb055b467ed8d3336f90904e
EnricAlmiñana <alminyana@gmail.com>
2015-12-10 20:43:54 +0100

    vegas library

---
+++ public/js/vegas/home_vegas.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$.vegas('slideshow', {
+	  delay:8000,  //tiempo que dura un background	
+	  transition: 'fade2', //fade por default
+	  shuffle: false,
+	  transitionDuration: 3000, //1000 default	
+	  backgrounds:[
+	    { src:'img/img-intro/base/IMG_4357.JPG', fade:2000 },  
+	    { src:'img/img-intro/base/IMG_2483.JPG', fade:2000 },
+	    { src:'img/img-intro/base/IMG_4684.JPG', fade:2000 },
+	    { src:'img/img-intro/base/IMG_5070.JPG', fade:2000 }
+	  ],
+	  //call back function. Se ejecuta después de cambiar de imagen
+	  // walk:function(step) {
+		 //    alert( 'N°' +step+ ' is now displayed' );
+		 //  }
+	})('overlay', {
+	  src:'overlays/10.png', //hay 15 tramados diferentes del 01.png al 15.png
+	  opacity: 1 //100% (0 = 0%)
+	});
+	//botones
+	// $('img').click(function(event){ 
+	// 	$.vegas('jump',event.target.id);
+	// 	//console.log(event.target.id);
+	// });
+});
